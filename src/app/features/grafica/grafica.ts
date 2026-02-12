ec050004b5dfaf0b7abdef6cfc4a61585f45c315
greciaivonne <20223l001052@utcv.edu.mx>
2026-02-12 12:52:52 -0600

    feat: se ajusto la paleta de colores en PSC y se genero nuevos componentes

---
+++ src/app/features/grafica/grafica.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafica',
+  imports: [],
+  templateUrl: './grafica.html',
+  styleUrl: './grafica.css',
+})
+export class Grafica {
+
+}
