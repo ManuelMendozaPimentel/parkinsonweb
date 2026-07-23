b5a09cdd9f562060cddb9a170face5c91740e172
Manuel Mendoza Pimentel <20213l001064@utcv.edu.mx>
2026-07-23 15:55:47 -0600

    Change API URL in PerfilService

--- src/app/services/perfil.ts
+++ src/app/services/perfil.ts
@@ -58,7 +58,7 @@ export interface ApiResponse<T = any> {
 
 @Injectable({ providedIn: 'root' })
 export class PerfilService {
-  private apiUrl = 'https://magapi-production.up.railway.app/api/perfil';
+  private apiUrl = 'http://neurotrack.us-east-2.elasticbeanstalk.com/api/perfil';
 
   constructor(
     private http: HttpClient,
